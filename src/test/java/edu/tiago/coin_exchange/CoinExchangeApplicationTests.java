ff514194ac6a9fbe355d673b139fe03a2d9b375d
brtiago <tgribeiro@gmail.com>
2024-06-24 10:18:04 -0300

    Initial commit

---
+++ src/test/java/edu/tiago/coin_exchange/CoinExchangeApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.tiago.coin_exchange;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoinExchangeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
